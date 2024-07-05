efb7913233f122b721164e3fcca2dc5ad05d4512
jewel hassan <jewelhassan399@gmail.com>
2024-07-05 15:13:56 +0600

    updated code

--- api/index.js
+++ api/index.js
@@ -1,48 +1,55 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
+import bodyParser from "body-parser";
+const port = 3000;
 dotenv.config();
 
+// MONGO DB CONNECT
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
   });
 
 const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// HOME ROUTE
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.use(express.json());
-
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-
+// SERVER ERROR
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     message,
     statusCode,
   });
+});
+
+// SERVER RUNNING
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
 });
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  apiKey: "AIzaSyBw3Hb9PHjbfBHUhpYXrm19oI-eWnNOwUw",
+  authDomain: "menrauth.firebaseapp.com",
+  projectId: "menrauth",
+  storageBucket: "menrauth.appspot.com",
+  messagingSenderId: "758148802342",
+  appId: "1:758148802342:web:a4fc4e4b75c0951576c3e7",
 };
 
 // Initialize Firebase
